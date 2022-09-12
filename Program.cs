b0f38570e5b88cd507d522f16568f8b579dcd597
Riickyii <krausz.miron@verebelyszki.hu>
2022-09-12 09:36:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Print logs? y/n");
+string l = Console.ReadLine().ToUpper();
+while (l != "Y")
+{
+    if (l == "N") { break; }
+    Console.WriteLine("Print logs? y/n");
+    l = Console.ReadLine().ToUpper();
+}
+bool s = false;
+if (l == "Y") { s = true; }
+Console.ReadKey();
